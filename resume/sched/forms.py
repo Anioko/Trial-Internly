0db0171ecea52caa73d7e802ece2bcfedbb12a7c
anioko <aniekanokono@gmail.com>
2014-05-22 20:37:34 +0300

    New files to test
    Added a folder called Resume.

---
+++ resume/sched/forms.py
@@ -0,0 +1,61 @@
+"""Forms to render HTML input & validate request data."""
+
+from wtforms import Form, BooleanField, DateTimeField, PasswordField
+from wtforms import TextAreaField, TextField
+from wtforms.validators import Length, required
+
+
+class AppointmentForm(Form):
+    """Render HTML input for Appointment model & validate submissions.
+
+    This matches the models.Appointment class very closely. Where
+    models.Appointment represents the domain and its persistence, this class
+    represents how to display a form in HTML & accept/reject the results.
+    """
+    title = TextField('Title', [Length(max=255)])
+    start = DateTimeField('Start', [required()])
+    end = DateTimeField('End')
+    allday = BooleanField('All Day')
+    location = TextField('Location', [Length(max=255)])
+    description = TextAreaField('Description')
+
+class ResumeForm(Form):
+    """Render HTML input for Resume model & validate submissions.
+
+    This matches the models.Resume class very closely. Where
+    models.Resume represents the domain and its persistence, this class
+    represents how to display a form in HTML & accept/reject the results.
+    """
+    name = TextField('Name', [Length(max=255)])
+    email = EmailField('Email', [Length(max=100)])
+    phone = TextField('Phone', [Length(max=255)])
+    city = TextField('City', [Length(max=100)])
+    zip = TextField('Zip', [Length(max=50)])
+    country = TextField('Country', [Length(max=255)])
+    url = TextField('Url', [Length(max=255)])
+    citizenship = TextField('Country of Birth', [Length(max=255)])
+
+    summary_title = TextField('Summary Title', [Length(max=255)])
+    summary_text = TextField('Summary Text', [Length(max=500)])
+
+    company_name = TextField('Company Name', [Length(max=255)])
+    company_summary = TextField('Company Summary', [Length(max=255)])
+    role = TextField('Role', [Length(max=255)])
+    role_description = TextAreaField('Role Description')
+    
+    
+    start = DateField('Start')
+    end = DateField('End')
+    currently = BooleanField('Currently')
+    location = TextField('Location', [Length(max=255)])
+
+
+
+
+class LoginForm(Form):
+    """Render HTML input for user login form.
+
+    Authentication (i.e. password verification) happens in the view function.
+    """
+    username = TextField('Username', [required()])
+    password = PasswordField('Password', [required()])
